ab1481876072523e2386d8c81c1fb20d387b9a83
EbinD <ebindevassykutty7@gmail.com>
2025-08-05 00:11:29 +0530

    Updated main.py with new logic

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 df = pd.read_csv("players.csv")
 
-# print(df.head())
+print(df.head())
 # print("Shape:", df.shape)
 # print("Columns:", df.index.tolist())
 # print("Missing values:\n", df.isnull().sum())
@@ -36,24 +36,24 @@ df = pd.read_csv("players.csv")
 
 
 # Set a clean style
-sns.set(style="whitegrid")
+# sns.set(style="whitegrid")
 
 # Plot
-plt.figure(figsize=(10, 6))
-sns.barplot(x="Goals", y="Name", data=df.sort_values(by="Goals"))
-plt.title("Goals Scored by Players")
-plt.xlabel("Goals")
-plt.ylabel("Player")
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(10, 6))
+# sns.barplot(x="Goals", y="Name", data=df.sort_values(by="Goals"))
+# plt.title("Goals Scored by Players")
+# plt.xlabel("Goals")
+# plt.ylabel("Player")
+# plt.tight_layout()
+# # plt.show()
 
 
 # Group and reset index so we can plot
-accuracy_by_pos = df.groupby("Position")["PassAccuracy"].mean().reset_index()
+# accuracy_by_pos = df.groupby("Position")["PassAccuracy"].mean().reset_index()
 
-plt.figure(figsize=(8, 5))
-sns.barplot(x="Position", y="PassAccuracy", data=accuracy_by_pos)
-plt.title("Avg Pass Accuracy by Position")
-plt.ylabel("Pass Accuracy (%)")
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(8, 5))
+# sns.barplot(x="Position", y="PassAccuracy", data=accuracy_by_pos)
+# plt.title("Avg Pass Accuracy by Position")
+# plt.ylabel("Pass Accuracy (%)")
+# plt.tight_layout()
+# plt.show()
