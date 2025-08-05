84a8b8ab1cbe721e34126a25b194ee369c55fdb4
EbinD <ebindevassykutty7@gmail.com>
2025-08-05 23:06:22 +0530

    Initial commit: cleaned data + EDA + README

---
+++ README.md
@@ -0,0 +1,30 @@
+# Football Player Stats – 2018/19
+
+This project analyzes player performance data from the 2018/19 football season.  
+It covers:
+
+- ✅ Data cleaning
+- 📊 Exploratory Data Analysis (EDA)
+- 🧠 Key insights on goals, positions, and efficiency
+
+## Project Structure
+
+football-analysis/
+    .git/
+    .gitignore
+    .ipynb_checkpoints/
+    main.py
+    notebooks/
+    players.csv
+    player_clean.csv
+    player_clean_step1.csv
+    raw_player.csv
+    README.md
+    venv/
+
+
+## Tools Used
+
+- Python 3.13
+- Pandas
+- Matplotlib / Seaborn
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-df = pd.read_csv("players.csv")
+import os
+# df = pd.read_csv("players.csv")
 
-print(df.head())
+# print(df.head())
 # print("Shape:", df.shape)
 # print("Columns:", df.index.tolist())
 # print("Missing values:\n", df.isnull().sum())
@@ -57,3 +59,40 @@ print(df.head())
 # plt.ylabel("Pass Accuracy (%)")
 # plt.tight_layout()
 # plt.show()
+
+# Load raw player data (real-world messy version)
+raw_df = pd.read_csv("raw_player.csv")
+# print(raw_df)
+
+# print("Shape:", raw_df.shape)
+# print("Columns:\n", raw_df.columns.tolist())
+# print("Data types:\n", raw_df.dtypes)
+# print("Missing values:\n", raw_df.isnull(
+# ).sum().sort_values(ascending=False).head(20))
+
+# 1. Count fully missing columns
+# fully_missing = raw_df.columns[raw_df.isnull().all()].tolist()
+# print(f"❌ Fully missing columns ({len(fully_missing)}):")
+# print(fully_missing)
+
+# 2. Show top 30 columns by missing values
+# missing_vals = raw_df.isnull().sum().sort_values(ascending=False)
+# print("\n🔍 Top 30 columns with most missing values:")
+# print(missing_vals.head(30))
+
+# 3. Count non-null columns (i.e. useful columns)
+# non_null_cols = raw_df.columns[raw_df.notnull().any()].tolist()
+# print(f"\n✅ Columns with at least some data: {len(non_null_cols)}")
+
+
+def print_top_level(path):
+    print(f"{os.path.basename(path)}/")
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            print(f"    {item}/")
+        else:
+            print(f"    {item}")
+
+
+print_top_level("C:/Users/solly/Desktop/football-analysis")
